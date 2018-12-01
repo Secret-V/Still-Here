a1b27d3c4d34c180eca47c7bc60e7e0080b84514
Vincent Booman <vincent_booman@msn.com>
2018-12-01 17:25:15 +0100

    First upload.

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float horizontalSpeed = 50.0f;
+    public float verticalSpeed = 40.0f;
+
+    public Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        Vector2 movement = Vector2.zero;
+
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            movement.x -= horizontalSpeed;
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            movement.x += horizontalSpeed;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            movement.y = verticalSpeed;
+        }
+
+        myRigidbody.AddForce(movement * Time.deltaTime);
+	}
+}
