1836b794772dd6e9830589f401f1dc0afb1bddd7
Vincent Booman <vincent_booman@msn.com>
2018-12-05 18:01:02 +0100

    Updated it to version we built in class.

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -7,6 +7,15 @@ public class Player : MonoBehaviour
     public float horizontalSpeed = 50.0f;
     public float verticalSpeed = 40.0f;
 
+    public float dashForceMultiplier = 5.0f;
+
+    public ForceMode2D dashForceMode;
+
+    public float maxFuel = 100.0f;
+    public float fuel = 100.0f;
+    public float fuelDecreasePerSecond = 50.0f;
+    public float fuelIncreasePerSecond = 100.0f;
+
     public Rigidbody2D myRigidbody;
 
 	// Use this for initialization
@@ -14,9 +23,14 @@ public class Player : MonoBehaviour
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update()
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 200), fuel.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         Vector2 movement = Vector2.zero;
 
@@ -28,11 +42,23 @@ public class Player : MonoBehaviour
         {
             movement.x += horizontalSpeed;
         }
-        if(Input.GetKey(KeyCode.Space))
+        if(Input.GetKey(KeyCode.Space) && fuel > .0f)
         {
             movement.y = verticalSpeed;
+            fuel -= fuelDecreasePerSecond * Time.deltaTime;
+            if(fuel < .0f) fuel = .0f;
+        }
+        else if(!Input.GetKey(KeyCode.Space))
+        {
+            fuel += fuelIncreasePerSecond * Time.deltaTime;
+            if(fuel > maxFuel) fuel = maxFuel;
         }
 
         myRigidbody.AddForce(movement * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            myRigidbody.AddForce(movement * dashForceMultiplier * Time.deltaTime, dashForceMode);
+        }
 	}
 }
